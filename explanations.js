c0ba93dd981fec10d373b3ae1ecc71c0659b240e
stillimaginarium <stillimaginarium@gmail.com>
2017-07-06 17:14:57 +0100

    made so cool stuff happen

---
+++ explanations.js
@@ -0,0 +1,39 @@
+//explanations
+var favColor = 'blue' ;
+var myArray = ['blue', 'red', 'green'];
+var numOfFavColors = 3;
+var hasGotFavColors = true;
+var richObject = {
+	firstName: 'Rich',
+	lastName: 'Armstrong',
+	favColors: ['blue', 'red', 'green'],
+	yearsAlive: 100,
+	isMale: true,
+};
+
+var whatIsMyFavColor = function () {
+	return 'blue';
+
+}
+
+var doSomething = function () {
+	console.log ("do Something!!!" )
+
+};
+
+var area = function (width, height) {
+	return width * height;
+
+
+}
+
+var fullName = function (firstname, secondname) {
+	return firstname + ' ' + secondname;
+
+
+}
+
+if (true) {
+	//do something
+	alert (male)
+}
